e116c4aa0f65b67fabb3030c162c0bc42f4c03ca
Linus <linus@wieland.lu>
2022-08-20 16:56:10 +0200

    monsters

---
+++ netlify/functions/get_monsters/get_monsters.ts
@@ -0,0 +1,62 @@
+
+import fetch from 'node-fetch'
+
+type Monster = {
+  index: string
+  name: string
+  url: string
+}
+
+type MonsterList = {
+  count?: number
+  results?: Monster[]
+}
+
+const DND_5E_API = 'https://www.dnd5eapi.co'
+
+async function getMonster() : Promise<Monster> {
+  const resp = await fetch(`${DND_5E_API}/api/monsters`)
+  const list: MonsterList | unknown = await resp.json()
+
+  const monsterList = list as MonsterList;
+  
+  if (!monsterList || !monsterList.count || !monsterList.results) {
+    throw new Error('no monsters')
+  }
+
+  return monsterList.results[Math.floor(Math.random() * monsterList.count)];
+}
+
+async function getMonsterData(url: string):Promise<unknown> {
+  try {
+    const resp = await fetch(DND_5E_API + url)
+    return resp.json()
+  } catch {
+    throw new Error('monster not found')
+  }
+}
+
+export async function handler() {
+  try {
+    const randomMonster = await getMonster()
+    const data = await getMonsterData(randomMonster.url)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application.json',
+      },
+      body: JSON.stringify({
+        message: err.message,
+      }),
+    }
+  }
+}
