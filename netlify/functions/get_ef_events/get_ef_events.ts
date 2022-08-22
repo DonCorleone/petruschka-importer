1d0dc0a3085d789b44bbddb1cd87b4f9c8ccd2ff
Linus <linus@wieland.lu>
2022-08-22 21:09:50 +0200

    EF Events

--- netlify/functions/get_ef_events/get_ef_events.ts
+++ netlify/functions/get_ef_events/get_ef_events.ts
@@ -3,13 +3,16 @@ import fetch from 'node-fetch'
 
 const API_URI = 'https://eventfrog.ch/api/web/events.modifyInfo.de.json?accessibleForAction=manage_event&distinctGroup=false&temporalState=future&page=1&perPage=50&sortBy=eventBegin&asc=true&state=draft&state=published&selector=organizer';
 
-export interface Movies {
-  id:         string;
-  modifyDate: Date | null;
+export interface EF_Event {
+  id: string;
+  modifyDate?: Date;
 }
 
+export interface EF_Event_Response {
+  events: EF_Event[];
+}
 
-async function getMonsterData():Promise<unknown> {
+async function getMonsterData():Promise<EF_Event[]> {
   try {
     var myHeaders: HeadersInit = {
       "Authorization": "Basic dml0b2Nvcmxlb25lNzdAZ21haWwuY29tOnJlcWppOS14ZXF6b2otcGlKZ2Vu",
@@ -21,7 +24,19 @@ async function getMonsterData():Promise<unknown> {
       headers: myHeaders,
       redirect: "follow",
     });
-    return resp.json()
+
+    const response = await resp.json() as EF_Event_Response;
+
+    const list: EF_Event[] | unknown = response.events;
+
+    const monsterList = list as EF_Event[];
+  
+    if (!monsterList || !monsterList.length) {
+      throw new Error('no monsters')
+    }
+  
+    return monsterList;
+
   } catch (err){
     throw new Error('froggy not found ' + err);
   }
