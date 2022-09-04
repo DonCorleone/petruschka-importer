64ae9a53feff6edd962f084786507f7f039b16c9
Linus <linus@wieland.lu>
2022-09-04 21:45:06 +0200

    data

--- netlify/functions/get_ef_events/get_ef_events.ts
+++ netlify/functions/get_ef_events/get_ef_events.ts
@@ -91,13 +91,13 @@ export async function insertEventIntoDb(
 export async function handler() {
   try {
     const data = await getEvents();
-    const result = await insertEventIntoDb(data);
+    //  const result = await insertEventIntoDb(data);
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(result)
+      body: JSON.stringify(data)
     };
   } catch (err) {
     return {
