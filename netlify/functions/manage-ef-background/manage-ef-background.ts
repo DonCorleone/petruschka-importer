777045c9c97d668468ff1133594faec808ba0495
Linus <linus@wieland.lu>
2022-09-06 20:42:00 +0200

    ef bg

--- netlify/functions/manage-ef-background/manage-ef-background.ts
+++ netlify/functions/manage-ef-background/manage-ef-background.ts
@@ -1,25 +1,57 @@
 import { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import {MongoClient, UpdateResult} from "mongodb";
+import {EF_Event_Detail} from "../../models/EF_Event_Detail";
+import getEvents from "../../services/efService";
+import getEventById from "../../services/efDetailService";
 
 export async function handler(event: HandlerEvent, context: HandlerContext) {
   try {
-    const sleep = (ms: number) => {
-      return new Promise((resolve) => setTimeout(resolve, ms));
-    };
+    const uri = process.env.MONGODB_URI;
+    const db = process.env.MONGODB_COLLECTION;
 
-    (async () => {
-      for (let i = 0; i <= 60; i++) {
-        const date = new Date();
-        await sleep(1000);
-        console.log(date.toLocaleString(), i);
-      }
-      console.log('Done');
-    })();
+    if (!(uri && db)) {
+      throw new Error('no mongo uri or db - env');
+    }
+
+    const mongoClient = new MongoClient(uri);
+    const clientPromise = mongoClient.connect();
+
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const collectionName = process.env.MONGODB_COLLECTION;
+
+    if (!collectionName) {
+      throw new Error('no collectionName - env');
+    }
+
+    const collection = database.collection<EF_Event_Detail>(collectionName);
+
+    const efEvents = await getEvents();
+
+    const asyncFunctions: Promise<UpdateResult>[] = [];
+
+    for (const efEvent of efEvents) {
+      console.log(JSON.stringify(efEvent));
+
+      const eventDetail = await getEventById(efEvent.id);
+
+      asyncFunctions.push(
+          collection.updateOne(
+              { id: efEvent.id },
+              { $set: { ...eventDetail.pop() } },
+              { upsert: true }
+          )
+      );
+    }
+
+    console.log('finito');
+
+    const result = await Promise.all(asyncFunctions);
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify('hello-background')
+      body: JSON.stringify(result)
     };
   } catch (err: any) {
     return {
